d94147075adf5ead96bf587fdd4bd1836dba26a4
developwizard <churkin.it@gmail.com>
2023-05-08 21:17:37 +0300

    Added dummy data at start of project

--- src/main/java/com/example/studentmanagementsystem/StudentManagementSystemApplication.java
+++ src/main/java/com/example/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -1,13 +1,32 @@
 package com.example.studentmanagementsystem;
 
+import com.example.studentmanagementsystem.entity.Student;
+import com.example.studentmanagementsystem.repository.StudentRepository;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
+    @Autowired
+    StudentRepository studentRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        if(studentRepository.findAll().isEmpty()) {
+            val students = new ArrayList<Student>();
+            students.add(new Student("Ivan", "Ivanov", "ivanov@gmail.com"));
+            students.add(new Student("Oleg", "Olegov", "olegov@gmail.com"));
+            students.add(new Student("Tatiana", "Tanushina", "tanushina@gmail.com"));
+            studentRepository.saveAll(students);
+        }
+    }
 }
